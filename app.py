bc5604de78041c4557b7bc96b6e8d0a80b543d35
Aman soni <113356532+Aman-bhai@users.noreply.github.com>
2022-12-31 11:35:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template,redirect,flash
+from django.shortcuts import render
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-model=pickle.load(open('regression_model.pkl','rb'))
-
+regmodel=pickle.load(open('regression_model.pkl','rb'))
+SS=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict.api',methods=['POST'])
 
+
 def predict_api():
+    return render_template('home.html')
     data=request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=SS.transform(np.array(list(data.values())).reshape(1,-1))   
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
     
